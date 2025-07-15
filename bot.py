146dfb1534fcd7812f7c5ead9a146d9fb2224610
Rico77-stax <aoxamubhenricho@gmail.com>
2025-07-15 15:22:41 +0200

    Delete bot.py

--- bot.py
+++
@@ -1,8 +0,0 @@
-from telegram import Update, InputFile
-from telegram.constants import ChatAction
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
-from telegram.helpers import escape_markdown
-import os
-from image_processor import extract_price_from_image, annotate_image
-from signal_engine import generate_signal
-from config import TELEGRAM_TOKEN, GROUP_ID
