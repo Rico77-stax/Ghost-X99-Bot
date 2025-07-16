a732cbc44e3099ea25cfcc98bd85a134512d18d5
Rico77-stax <aoxamubhenricho@gmail.com>
2025-07-16 09:20:41 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
+# config.py
 
+# Ghost X99 – Quantum Lion Vortex Bot Configuration
+
+BOT_TOKEN = "7974220853:AAE80t4o5-3UpZjRCaGDnnRcIMb0ZKbtXrk"
+GROUP_ID = -1002824996503  # Telegram group ID where the bot posts signals
